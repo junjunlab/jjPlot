4a27a4ca331c360639ff5c52552f4d1c70d932d6
JunJunLao <1138976957@qq.com>
2023-08-30 20:44:20 +0800

    add grid.colorkey

--- R/grid_funcs.R
+++ R/grid_funcs.R
@@ -129,3 +129,100 @@ grid.yaxis2 <- function(at = NULL,
             just = text.just,
             gp = gpar(fontsize = label.size))
 }
+
+
+#' Create a Color Key Grid
+#'
+#' This function creates a color key grid using the grid graphics system.
+#'
+#' @param x Numeric vector specifying the range for the x-axis.
+#' @param color Vector of color values to be used for the color key.
+#' @param color.n Number of colors to generate.
+#' @param ticks.side Side of the color key to display tick marks
+#' ("left", "right", "top", or "bottom").
+#' @param pos Position of the color key ("h" for horizontal, "v" for vertical).
+#' @return NULL
+#'
+#' @importFrom grid grid.rect gpar
+#' @importFrom grDevices colorRampPalette
+#'
+#' @export
+grid.colorkey <- function(x = NULL,
+                          color = NULL,
+                          color.n = 100,
+                          ticks.side = c("left","right","top","bottom"),
+                          pos = c("h","v")){
+  pos <- match.arg(pos,c("h","v"))
+  ticks.side <- match.arg(ticks.side,c("left","right","top","bottom"))
+
+  # check position
+  if(pos == "v"){
+    x_scale <- c(0,1)
+    y_scale <- range(as.numeric(x))
+
+    xpos <- 0.5
+    ypos <- seq(0,1, length = color.n)
+
+    r_width = unit(1, "npc")
+    r_height = 1/(color.n - 1)
+  }else{
+    y_scale <- c(0,1)
+    x_scale <- range(as.numeric(x))
+
+    ypos <- 0.5
+    xpos <- seq(0,1, length = color.n)
+
+    r_width = 1/(color.n - 1)
+    r_height = unit(1, "npc")
+  }
+
+  # canvas
+  pushViewport(viewport(angle = 0,
+                        yscale = y_scale,
+                        xscale = x_scale))
+  # assign colors
+  if(is.null(color)){
+    cols <- c("blue","white","red")
+  }else{
+    cols <- color
+  }
+
+  col_p <- colorRampPalette(cols)(color.n)
+
+  # just
+  if(pos == "v"){
+    just <- c("bottom",rep("centre",color.n-2),"top")
+
+    # loop to create color
+    for (i in 1:color.n) {
+      grid.rect(x = xpos,
+                y = ypos[i],
+                height = r_height,
+                width = r_width,
+                just = just[i],
+                gp = gpar(col = col_p[i], fill = col_p[i]))
+    }
+  }else{
+    just <- c("left",rep("centre",color.n-2),"right")
+
+    # loop to create color
+    for (i in 1:color.n) {
+      grid.rect(x = xpos[i],
+                y = ypos,
+                height = r_height,
+                width = r_width,
+                just = just[i],
+                gp = gpar(col = col_p[i], fill = col_p[i]))
+    }
+  }
+
+  grid.rect(gp = gpar(fill = NA))
+
+  # add axis
+  if(pos == "h"){
+    jjPlot::grid.xaxis2(side = ticks.side,tick.len = 0.25)
+  }else{
+    jjPlot::grid.yaxis2(side = ticks.side,tick.len = 0.25)
+  }
+  popViewport()
+}
